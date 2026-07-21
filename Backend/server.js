8c49b4a103c0fdc8eb93e5750c669ddc73e46f1f
daro128 <p8735036@gmail.com>
2026-07-21 16:51:32 +0700

    fix render port

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 5000
 console.log("BOT_TOKEN:", process.env.BOT_TOKEN);
 console.log("CHAT_ID:", process.env.CHAT_ID);
 
-app.listen(PORT, () => {
-    console.log(`Backend listening on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Backend listening on port ${port}`);
 });
